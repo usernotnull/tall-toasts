e821087144e2296b67a007eb02cc7bf97ed663d0
usernotnull <15612814+usernotnull@users.noreply.github.com>
2021-10-28 19:56:56 +0300

    Update WireToast.php

--- src/Concerns/WireToast.php
+++ src/Concerns/WireToast.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 
 namespace Usernotnull\Toast\Concerns;
 
-use Livewire\Response;
 use Usernotnull\Toast\ToastManager;
 
 trait WireToast
 {
-    public function dehydrate(Response $response): void
+    public function dehydrate(): void
     {
         if (! ToastManager::componentRendered()) {
             foreach (ToastManager::pull() ?? [] as $notification) {
